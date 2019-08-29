f424401e363fdb12af7e1f594426beba417a1ce0
manuusc <septembermanu@gmail.com>
2019-08-29 03:01:49 -0700

    adding back to home button

--- src/app/createsuccess/createsuccess.page.ts
+++ src/app/createsuccess/createsuccess.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { HomePage } from '../home/home.page';
+
 
 @Component({
   selector: 'app-createsuccess',
   templateUrl: './createsuccess.page.html',
   styleUrls: ['./createsuccess.page.scss'],
 })
-export class CreatesuccessPage implements OnInit {
 
-  constructor() { }
+export class CreatesuccessPage implements OnInit {
+  homepage: HomePage;
+
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  gotoHomePage(){
+    this.navCtrl.navigateBack('/home');
   }
 
 }
